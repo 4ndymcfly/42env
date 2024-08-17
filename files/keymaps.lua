77d3a0ae1eec97bd82c6f4d3cc892c4f76cc13a6
Andrés Lorente <30553433+4ndymcfly@users.noreply.github.com>
2024-08-17 19:52:17 +0200

    Update keymaps.lua

--- files/keymaps.lua
+++ files/keymaps.lua
@@ -23,4 +23,4 @@ vim.api.nvim_set_keymap('n', '<F4>', ':q<CR>', { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<F5>', ':w<CR>', { noremap = true, silent = true })
 
 -- Mapear F6 para abrir/cerrar el explorador de NeoTree en el directorio de trabajo actual (cwd)
-vim.api.nvim_set_keymap('n', '<F6>', '<cmd>Neotree toggle current<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F6>', '<cmd>Neotree toggle<CR>', { noremap = true, silent = true })
